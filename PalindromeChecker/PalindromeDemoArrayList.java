8f75d0f04c6296dff69a32000bf42806796f1f46
Peyton Hall <132291348+Peytonjohnhall@users.noreply.github.com>
2023-11-30 13:49:05 -0600

    Update and rename PalindromeDemo.java to PalindromeDemoArrayList.java

--- PalindromeChecker/PalindromeDemoArrayList.java
+++ PalindromeChecker/PalindromeDemoArrayList.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class PalindromeDemo 
+public class PalindromeDemoArrayList 
 {
     public static void main(String[] args) 
     {
