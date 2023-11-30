6bbaa22efdeea819ff93a4e691d1f770e9476115
Peyton Hall <132291348+Peytonjohnhall@users.noreply.github.com>
2023-11-30 11:54:34 -0600

    Update Airplane.java

--- AirportSimulation/Airplane.java
+++ AirportSimulation/Airplane.java
@@ -9,12 +9,12 @@ public class Airplane
     private int currentTime;
 
     /** 
-    	@param startTime is an integer representing the start
-    					 time of the airplane.
-		Precondition:    startTime is a non-negative integer.
-		Postcondition:   An instance of the Airplane class is created
-						 with a start time.
-	*/
+    	@param  	 startTime is an integer representing the start
+    			 time of the airplane.
+	Precondition:    startTime is a non-negative integer.
+	Postcondition:   An instance of the Airplane class is created
+			 with a start time.
+    */
     public Airplane(int startTime)
     {
     	// The constructor will save the startTime into the field
@@ -23,10 +23,10 @@ public class Airplane
     }
 
     /** 
-		Precondition:  the airplane has entered a queue and the time in has
-					   been saved into the feld.
-		Postcondition: the timelnQueue has been returned.
-	*/
+    	Precondition:  the airplane has entered a queue and the time in has
+		       been saved into the feld.
+	Postcondition: the timelnQueue has been returned.
+    */
     public int getTimeInQueue()
     {
     	// Getter to retrieve the amount of time 
@@ -36,11 +36,11 @@ public class Airplane
 
     /** 
     	@param startTime is an integer representing the time in which
-    					 the airplane entered the queue.
-		Precondition:    None.
-		Postcondition:   The timeInQueue field of the Airplane object is
-					     set to the startTime value.
-	*/
+    			 the airplane entered the queue.
+	Precondition:    None.
+	Postcondition:   The timeInQueue field of the Airplane object is
+			 set to the startTime value.
+    */
     public void setTimeInQueue(int startTime)
     {
     	// Setter to set the amount of time 
