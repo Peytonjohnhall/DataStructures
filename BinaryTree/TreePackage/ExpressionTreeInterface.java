765155bb769f4000d5c5fb45ca036eeafa9edac5
Peyton Hall <132291348+Peytonjohnhall@users.noreply.github.com>
2023-11-30 23:09:33 -0600

    Add files via upload

---
+++ BinaryTree/TreePackage/ExpressionTreeInterface.java
@@ -0,0 +1,12 @@
+package TreePackage;
+/**
+   An interface for an expression tree.
+*/
+public interface ExpressionTreeInterface 
+                 extends BinaryTreeInterface<String>
+{
+   /** Computes the value of the expression in this tree.
+      @return  The value of the expression. 
+   */
+   public double evaluate();
+} // end ExpressionTreeInterface
---
+++ BinaryTree/TreePackage/GeneralTreeInterface.java
@@ -0,0 +1,20 @@
+package TreePackage;
+
+/**
+   An interface for the ADT general tree.
+*/
+public interface GeneralTreeInterface<T> extends TreeInterface<T>,
+                                                 TreeIteratorInterface<T>
+{
+   /** Sets the data in the root of this general tree.
+      @param rootData  The object that is the data for the tree's root. 
+   */
+   public void setRootData(T rootData);
+   
+   /** Sets the root data and the children of this general tree.
+      @param rootData  An object that is the data in the root of
+                       the new tree.
+      @param children  An array of subtrees of the root. 
+   */
+   public void setTree(T rootData, GeneralTreeInterface<T>[] children);
+} // end GeneralTreeInterface
